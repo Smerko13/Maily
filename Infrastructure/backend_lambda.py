ace4ce2b7a52f1baa21f7af81f1f914f593b74d3
Daniel Smerkovich <danielsmerko13@gmail.com>
2026-03-03 22:39:55 +0200

    first dynamodb check

--- Infrastructure/backend_lambda.py
+++ Infrastructure/backend_lambda.py
@@ -1,8 +1,28 @@
 import json
+import boto3
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Maily-Emails')
 
 def lambda_handler(event, context):
-    print("Received event: ", event)
-    return {
-        'statusCode': 200,
-        'body': json.dumps("Hello from Maily Backend! The lambda is working.")
-    }
+    try:
+        email_id = str(uuid.uuid4())
+        item = {
+            'userId': 'daniel_test_user_1',
+            'emailId': email_id,
+            'subject': 'First Test Email',
+            'content': 'This email was written to the database directly from our API Gateway and Lambda!',
+            'status': 'processed'
+        }
+        table.put_item(Item=item)
+        return {
+            'statusCode': 200,
+            "body": json.dumps(f"Success! Email record {email_id} was saved to DynamoDB.")
+        }
+    except Exception as e:
+        print(f"Error saving to DynamoDB: {str(e)}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps("Failed to save data. Check Lambda logs.")
+        }
